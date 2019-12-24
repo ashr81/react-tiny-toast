ed07ed77e0e688d71926e92636473f829d70689e
ashr81 <ashrithreddi81@gmail.com>
2019-12-24 13:53:45 +0530

    Adds removed lib files

---
+++ packages/react-tiny-toast/lib/index.js
@@ -0,0 +1,16 @@
+export { default as toast } from './toast';
+export { default as ToastContainer } from './ToastContainer';
+export const POSITIONS = {
+  TOP_CENTER: 'top-center',
+  TOP_LEFT: 'top-left',
+  TOP_RIGHT: 'top-right',
+  BOTTOM_LEFT: 'bottom-left',
+  BOTTOM_RIGHT: 'bottom-right',
+  BOTTOM_CENTER: 'bottom-center'
+};
+export const VARIANTS = {
+  SUCCESS: 'success',
+  DANGER: 'danger',
+  WARNING: 'warning',
+  DEFAULT: 'default'
+};
